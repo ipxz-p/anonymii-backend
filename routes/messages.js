9362a45a995e3cfaf57fa3fc870758a85e500cda
Pongsiri Suwannasilp <88420839+ipxz-p@users.noreply.github.com>
2024-02-27 21:11:23 +0700

    feat: 🎸 add messages api doc

--- routes/messages.js
+++ routes/messages.js
@@ -2,8 +2,47 @@ import express from "express"
 import { getMessages, sendMessage } from "../controllers/messages.js"
 
 const router = express.Router()
-
+/**
+ * @openapi
+ * /message/sendMessage:
+ *  post:
+ *     tags:
+ *     - chat
+ *     description: sendMessage
+ *     parameters:
+ *      - name: message
+ *        in: body
+ *        description: message
+ *        required: true
+ *      - name: chatId
+ *        in: body
+ *        description: chatId
+ *        required: true
+ *      - name: senderEmail
+ *        in: body
+ *        description: senderEmail
+ *        required: true
+ *     responses:
+ *       200:
+ *         description: Success
+ */
 router.post("/sendMessage", sendMessage)
+/**
+ * @openapi
+ * /message/getMessages:
+ *  get:
+ *     tags:
+ *     - chat
+ *     description: getMessages
+ *     parameters:
+ *      - name: chatId
+ *        in: params
+ *        description: chatId
+ *        required: true
+ *     responses:
+ *       200:
+ *         description: Success
+ */
 router.get('/getMessages', getMessages)
 
 export default router
